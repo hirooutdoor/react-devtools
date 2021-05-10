9c12c415c2e9cf4550537d5cd6ea273c89259df3
hiro <>
2021-05-11 05:09:47 +0900

    add map() to contact and remove redundant Cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,25 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
-import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar
-        img="https://www.wantedly.com/users/140950872/avatar?h=500&t=1612876248&w=500"
-        />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,8 +6,9 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+      <p>{props.id}</p>
         <h2 className="name">{props.name}</h2>
-        <Avatar img={props.img}/>
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
         <Details detailInfo={props.tel} />
